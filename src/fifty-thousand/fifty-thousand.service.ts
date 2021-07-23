e26cc1c29f216286fee9ca6dfc2d8ebd3a3aee67
月海 <spectres@foxmail.com>
2021-07-23 13:56:28 +0800

    add db init logic

--- src/fifty-thousand/fifty-thousand.service.ts
+++ src/fifty-thousand/fifty-thousand.service.ts
@@ -1,15 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Gender } from '../common/constants/gender';
 import { FiftyThousandEntity } from '../entities/fifty-thousand.entity';
 
 @Injectable()
-export class FiftyThousandService {
+export class FiftyThousandService implements OnModuleInit {
   constructor(
     @InjectRepository(FiftyThousandEntity)
     private readonly fiftyThousandRepository: Repository<FiftyThousandEntity>
   ) {}
+
+  async onModuleInit() {
+    console.log('fifty-thousand-service');
+    const totalCounts = 50000;
+    const totalRows = await this.fiftyThousandRepository.count();
+    const oneBatch = 5000;
+    const diff = totalCounts - totalRows;
+    if (diff === 0) return console.log(`已满${totalCounts}条`);
+    if (diff > 0) {
+      const mod = diff % oneBatch;
+      const circles = Math.floor(diff / oneBatch);
+      const entity = new FiftyThousandEntity();
+      entity.name = '测试';
+      entity.age = 0;
+      entity.gender = Gender.Male;
+      for (let i = 0; i < circles; i++) {
+        const rowList = [];
+        for (let i = 0; i < oneBatch; i++) {
+          rowList.push(entity);
+        }
+        await this.fiftyThousandRepository.save(rowList);
+        console.log('插入一批');
+      }
+      const rowList = [];
+      for (let i = 0; i < mod; i++) {
+        rowList.push(entity);
+      }
+      await this.fiftyThousandRepository.save(rowList);
+    } else {
+      const mod = -diff % oneBatch;
+      const circles = Math.floor(-diff / oneBatch);
+      for (let i = 0; i < circles; i++) {
+        const rowList = await this.fiftyThousandRepository.createQueryBuilder('ten_thousands').limit(oneBatch).getMany();
+        Promise.all(
+          rowList.map(async item => {
+            this.fiftyThousandRepository.delete(item.uuid);
+          })
+        );
+      }
+      const rowList = await this.fiftyThousandRepository.createQueryBuilder('ten_thousands').limit(mod).getMany();
+      Promise.all(
+        rowList.map(async item => {
+          this.fiftyThousandRepository.delete(item.uuid);
+        })
+      );
+    }
+  }
 
   async findOneByUuid(uuid: string): Promise<FiftyThousandEntity> {
     return this.fiftyThousandRepository.findOne(uuid);
--- src/hundred-thousand/hundred-thousand.service.ts
+++ src/hundred-thousand/hundred-thousand.service.ts
@@ -1,13 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Gender } from '../common/constants/gender';
 import { HundredThousandEntity } from '../entities/hundred-thousand.entity';
 
 @Injectable()
-export class HundredThousandService {
+export class HundredThousandService implements OnModuleInit {
   @InjectRepository(HundredThousandEntity)
   private readonly hundredThousandRepository: Repository<HundredThousandEntity>;
+
+  async onModuleInit() {
+    console.log('hundred-thousand-service');
+    console.log('fifty-thousand-service');
+    const totalCounts = 100000;
+    const totalRows = await this.hundredThousandRepository.count();
+    const oneBatch = 10000;
+    const diff = totalCounts - totalRows;
+    if (diff === 0) return console.log(`已满${totalCounts}条`);
+    if (diff > 0) {
+      const mod = diff % oneBatch;
+      const circles = Math.floor(diff / oneBatch);
+      const entity = new HundredThousandEntity();
+      entity.name = '测试';
+      entity.age = 0;
+      entity.gender = Gender.Male;
+      for (let i = 0; i < circles; i++) {
+        const rowList = [];
+        for (let i = 0; i < oneBatch; i++) {
+          rowList.push(entity);
+        }
+        await this.hundredThousandRepository.save(rowList);
+        console.log('插入一批');
+      }
+      const rowList = [];
+      for (let i = 0; i < mod; i++) {
+        rowList.push(entity);
+      }
+      await this.hundredThousandRepository.save(rowList);
+    } else {
+      const mod = -diff % oneBatch;
+      const circles = Math.floor(-diff / oneBatch);
+      for (let i = 0; i < circles; i++) {
+        const rowList = await this.hundredThousandRepository.createQueryBuilder('ten_thousands').limit(oneBatch).getMany();
+        Promise.all(
+          rowList.map(async item => {
+            this.hundredThousandRepository.delete(item.uuid);
+          })
+        );
+      }
+      const rowList = await this.hundredThousandRepository.createQueryBuilder('ten_thousands').limit(mod).getMany();
+      Promise.all(
+        rowList.map(async item => {
+          this.hundredThousandRepository.delete(item.uuid);
+        })
+      );
+    }
+  }
 
   async findOneByUuid(uuid: string): Promise<HundredThousandEntity> {
     return this.hundredThousandRepository.findOne(uuid);
